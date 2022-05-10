63315141287ecc5f499282b94532fc720593b576
Jamil Bousquet <jamil.bousquet@gmail.com>
2022-05-10 18:13:31 -0400

    added historical price data for attainable assets

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-from libs.extract import get_treasury_assets
+from libs.extract import (
+    get_treasury_portfolio, get_treasury,
+    get_historical_price_by_address, get_historical_price_by_symbol
+)
