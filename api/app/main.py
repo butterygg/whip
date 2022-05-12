3a9844b8eed6d522649e60fe2cda29dbbd4d3342
lajarre <alexandre.hajjar@gmail.com>
2022-05-12 20:22:51 +0100

    ⚙️  Restructure backend files w/ Docker (fastpi + redis)

---
+++ api/app/main.py
@@ -0,0 +1,13 @@
+import os
+
+import redis
+from fastapi import FastAPI
+
+from libs.extract import (get_historical_price_by_address,
+                          get_historical_price_by_symbol,
+                          get_token_transfers_for_wallet, get_treasury,
+                          get_treasury_portfolio)
+from libs.pd_inter_calc import portfolio_filler
+
+app = FastAPI()
+db = redis.StrictRedis(host=os.environ["REDIS_HOST"])
