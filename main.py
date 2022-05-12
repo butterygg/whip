0d9532b53cdb79ef1b19767a1baeceb9ca6dcf3a
lajarre <alexandre.hajjar@gmail.com>
2022-05-12 20:22:51 +0100

    ⚙️  Restructure backend files w/ Docker (fastpi + redis)

--- main.py
+++
@@ -1,7 +0,0 @@
-from libs.extract import (
-    get_treasury_portfolio, get_treasury,
-    get_historical_price_by_address, get_historical_price_by_symbol,
-    get_token_transfers_for_wallet
-)
-
-from libs.pd_inter_calc import portfolio_filler
