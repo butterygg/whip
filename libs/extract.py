edffec1378da8d9eb490a77b4ff640c79d137d8a
Jamil Bousquet <jamil.bousquet@gmail.com>
2022-05-13 07:11:53 -0400

    getting token transfers for treasuries

--- libs/extract.py
+++ libs/extract.py
@@ -16,6 +16,15 @@ async def get_treasury_portfolio(treasury_address: str, chain_id: Optional[int] 
 
         return resp.json()["data"]
 
+async def get_token_transfers_for_wallet(treasury_address: str, contract_address: str, chain_id: Optional[int] = 1) -> Dict[str, Any]:
+    async with AsyncClient() as client:
+        resp = await client.get(
+            f"https://api.covalenthq.com/v1/{chain_id}/address/{treasury_address}" +\
+                f"/transfers_v2/?quote-currency=USD&format=JSON" +\
+                f"&contract-address={contract_address}&key=ckey_{getenv('COVALENT_KEY')}"
+        )
+    return resp.json()["data"]
+
 async def get_historical_price_by_symbol(
     token_symbol: str,
     start_date: str,
