7ec29fb5a29eb54c1f64347ffa01b01b64821792
Jamil Bousquet <jamil.bousquet@gmail.com>
2022-05-20 10:44:52 -0400

    renamed inter calc lib

--- api/app/main.py
+++ api/app/main.py
@@ -7,7 +7,7 @@ from libs.extract import (get_historical_price_by_address,
                           get_historical_price_by_symbol,
                           get_token_transfers_for_wallet, get_treasury,
                           get_treasury_portfolio)
-from libs.pd_inter_calc import portfolio_filler
+from libs.pd_inter_calc import erc20_portfolio_filler
 
 app = FastAPI()
 db = redis.StrictRedis(host=os.environ["REDIS_HOST"])
