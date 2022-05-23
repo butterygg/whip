e700a8f192b809f71bb04ecaf489221a9d632545
Jamil Bousquet <jamil.bousquet@gmail.com>
2022-05-23 18:54:20 -0400

    reformatted long strings

--- scheduler/app/libs/coingecko/coins.py
+++ scheduler/app/libs/coingecko/coins.py
@@ -75,7 +75,9 @@ async def get_coin_hist_price(
     timeout = Timeout(10.0, read=15.0, connect=30.0)
     async with AsyncClient(
         headers={
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50"
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+                          "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                          "Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50"
         }
     ) as client:
         # replace ETH address to WETH
@@ -83,13 +85,17 @@ async def get_coin_hist_price(
             contract_address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
         try:
             resp = await client.get(
-                f"https://api.coingecko.com/api/v3/coins/ethereum/contract/{contract_address}/market_chart/range?vs_currency=usd&from={start}&to={end}",
+                f"https://api.coingecko.com/api/v3/coins/ethereum/"
+                    + f"contract/{contract_address}/market_chart/"
+                    + f"range?vs_currency=usd&from={start}&to={end}",
                 timeout=timeout,
             )
         except MaybeEncodingError as e:
             sleep(5)
             resp = await client.get(
-                f"https://api.coingecko.com/api/v3/coins/ethereum/contract/{contract_address}/market_chart/range?vs_currency=usd&from={start}&to={end}",
+                f"https://api.coingecko.com/api/v3/coins/ethereum/contract/"
+                    + f"{contract_address}/market_chart/"
+                    + f"range?vs_currency=usd&from={start}&to={end}",
                 timeout=timeout,
             )
         sleep(5)
