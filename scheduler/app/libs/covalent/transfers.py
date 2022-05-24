12f11a9af87bf40f0335db8db307bb97e1fa942a
lajarre <alexandre.hajjar@gmail.com>
2022-05-24 14:46:33 +0100

    🐎 Cache Covalent token transfers

--- scheduler/app/libs/covalent/transfers.py
+++ scheduler/app/libs/covalent/transfers.py
@@ -1,12 +1,30 @@
 from os import getenv
-
+import json
 from typing import Any, Dict, Optional
+import dateutil
+from pytz import UTC
 from httpx import AsyncClient, Timeout
+
+from .. import db
+
+CACHE_HASH = "covalent_transfers"
+CACHE_KEY_TEMPLATE = "{treasury_address}_{contract_address}_{chain_id}_{date}"
 
 
 async def get_token_transfers_for_wallet(
     treasury_address: str, contract_address: str, chain_id: Optional[int] = 1
 ) -> Dict[str, Any]:
+    cache_date = dateutil.utils.today(UTC).strftime("%Y-%m-%d")
+    cache_key = CACHE_KEY_TEMPLATE.format(
+        treasury_address=treasury_address,
+        contract_address=contract_address,
+        chain_id=chain_id,
+        date=cache_date,
+    )
+
+    if db.hexists(CACHE_HASH, cache_key):
+        return json.loads(db.hget(CACHE_HASH, cache_key))
+
     timeout = Timeout(10.0, read=15.0, connect=30.0)
     async with AsyncClient(timeout=timeout) as client:
         resp = await client.get(
@@ -14,4 +32,8 @@ async def get_token_transfers_for_wallet(
             + f"/transfers_v2/?quote-currency=USD&format=JSON"
             + f"&contract-address={contract_address}&key=ckey_{getenv('COVALENT_KEY')}"
         )
-    return resp.json()["data"]
+    data = resp.json()["data"]
+
+    db.hset(CACHE_HASH, cache_key, json.dumps(data))
+
+    return data
