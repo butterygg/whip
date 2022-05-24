99886d2239283646f63cc6cbbb3cb56a99cbbba3
lajarre <alexandre.hajjar@gmail.com>
2022-05-24 19:50:08 +0100

    🧮 Spread USDC backtesting (only non-USDC portfolios for now)
    * Restructure front-end code for API calls
    * Call new backend endpoint
    * Front-end refactoring to comply with backend types
    * Spread enpoint for portfolios with 0 USDC

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState, createContext, useContext } from "react";
 import { Line } from "react-chartjs-2";
 import logo from "./whipped-cream.png";
 import "./App.css";
@@ -40,9 +40,9 @@ type AssetsBreakdown = Record<
   }
 >;
 type Kpis = {
-  "total value"?: number;
-  volatility?: number;
-  "return vs market"?: number | "Infinity";
+  totalValue: number;
+  volatility: number;
+  returnVsMarket: number | "Infinity";
 };
 
 const getTodayMidnight = () => {
@@ -67,16 +67,21 @@ function deltaDate(input: Date, years: number, months: number, days: number) {
   );
 }
 
+type SimulationContext = {
+  address: string;
+  startDate: Date;
+};
+const SimulationReactContext = createContext<SimulationContext>({
+  address: "",
+  startDate: deltaDate(getTodayMidnight(), -1, 0, 0),
+});
+
 function App() {
   const today = getTodayMidnight();
 
   const [address, setAddress] = useState("");
   const [startDate, setStartDate] = useState(deltaDate(today, -1, 0, 0));
-  const [baseKpis, setBaseKpis] = useState({
-    "total value": undefined,
-    volatility: undefined,
-    "return vs market": undefined,
-  } as Kpis);
+  const [baseKpis, setBaseKpis] = useState(undefined as Kpis | undefined);
   const [newKpis, setNewKpis] = useState(undefined as Kpis | undefined);
   const [baseAssets, setBaseAssets] = useState({} as AssetsBreakdown);
   const [newAssets, setNewAssets] = useState(
@@ -88,7 +93,6 @@ function App() {
     datasets: [],
   } as ChartData<"line">);
   const [openedProduct, setOpenedProduct] = useState<undefined | number>();
-  const [activeProduct, setActiveProduct] = useState<undefined | number>();
 
   const products = [
     {
@@ -175,193 +179,144 @@ function App() {
     })();
   }, [address, startDate]);
 
-  // Fetch product endpoint
-  useEffect(() => {
-    (async () => {
-      if (typeof activeProduct === "undefined") return;
-      setNewKpis({
-        "total value": 1_433_489_765,
-        volatility: 0.044,
-        "return vs market": 0.18,
-      });
-      setNewAssets({
-        UNI: {
-          allocation: 0.8,
-          volatility: 0.052,
-          riskContribution: 1,
-        },
-        USDC: {
-          allocation: 0.2,
-          volatility: 0.0001,
-          riskContribution: 0,
-        },
-      });
-      setChartData({
-        labels: chartData.labels,
-        datasets: [
-          chartData.datasets[0],
-          {
-            label: "Diversified portfolio",
-            backgroundColor: "rgb(213, 175, 8)",
-            borderColor: "rgb(213, 175, 8)",
-            data: [
-              501.07393327578797, 490.6410219873329, 526.116181498266,
-              556.1416890647487, 548.251294489849, 535.2675166295701,
-              514.0556126901168, 481.21387735414595, 491.28428090651,
-              467.5892338269915, 461.11240474293504, 482.7718449901471,
-              498.73311219808846, 476.1547586872153, 470.4765765726248,
-              468.0430816211164, 454.98006017236213, 467.53563130749257,
-              477.2648378101033, 494.1791636967664, 500.0377285818866,
-              518.1742879375948, 520.4509949950595, 503.99443288253536,
-              504.8270847150209, 522.7609328803725, 531.6231813700321,
-              566.1493525054457, 559.2137360768705, 558.9690093241638,
-              592.7064764180922, 572.6156743492405, 586.9227769180984,
-              608.0778594315306, 583.9805943156259, 596.3367267194044,
-              599.75112388648, 608.2987968142213, 594.3045721922251,
-              577.4356304483623, 527.5236112948955, 551.1490636919361,
-              516.4442175065085, 531.9634494858849, 525.0219721443382,
-              485.2627255964389, 500.9157594378702, 515.1532813592157,
-              507.0444838303761, 519.9932956507782, 515.6290050312157,
-              497.09036273666374, 503.9365870505458, 509.67769550147807,
-              499.95123859773327, 480.15228478549034, 482.7417093976729,
-              483.2980097095981, 473.9123871048367, 475.61336248673024,
-              443.03662635063324, 452.4372681800581, 450.3482925209935,
-              427.6846285839989, 391.903127107845, 406.0846186391889,
-              398.14439994229474, 394.5577618201554, 442.10384613494267,
-              414.8892370147741, 427.58167047837554, 408.82704761155554,
-              405.03686024343085, 358.89775797088123, 360.48968257303403,
-              308.98050270014096, 301.13853977708015, 318.24262446730353,
-              323.1975651311278, 455.5546491304233, 429.17778651931525,
-              458.16388361772465, 419.10052553060063, 439.12421126277434,
-              433.6958126589046,
-            ],
-          },
-        ],
-      });
-    })();
-  }, [activeProduct]);
-
   const resetAssets = () => {
     setNewKpis(undefined);
     setNewAssets(undefined);
     setChartData({
       labels: chartData.labels,
-      datasets: [chartData.datasets[0]],
+      datasets: chartData.datasets.length === 0 ? [] : [chartData.datasets[0]],
+    });
+  };
+
+  const previewNewChartData = (newData: Record<string, number>) => {
+    setChartData({
+      labels: chartData.labels,
+      datasets: [
+        chartData.datasets[0],
+        {
+          label: "Diversified portfolio",
+          backgroundColor: "rgb(213, 175, 8)",
+          borderColor: "rgb(213, 175, 8)",
+          data: Object.values(newData).map((x) => (x as number) / 1_000_000),
+        },
+      ],
     });
   };
 
   return (
-    <div>
-      <header className="p-2 bg-[#ddd] flex justify-between items-center">
-        <div className="w-[15em]"></div>
-        <img className="h-[100px]" src={logo} alt="logo" />
-        <input
-          className="m-3 w-[15em] p-2"
-          placeholder={address || "Address"}
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              setAddress(event.currentTarget.value);
-            }
-          }}
-        ></input>
-      </header>
-      <div className="flex items-center justify-center">
-        <div className="bg-[#eee] w-2/3 m-10">
-          <div className="w-full bg-[#ddd] p-4 text-white  ">
-            <div className="flex items-center justify-center">
-              <Line
-                data={chartData}
-                options={{
-                  responsive: true,
-                  plugins: {
-                    legend: {
-                      position: "top" as const,
+    <SimulationReactContext.Provider
+      value={{ address: address, startDate: startDate }}
+    >
+      <div>
+        <header className="p-2 bg-[#ddd] flex justify-between items-center">
+          <div className="w-[15em]"></div>
+          <img className="h-[100px]" src={logo} alt="logo" />
+          <input
+            className="m-3 w-[15em] p-2"
+            placeholder={address || "Address"}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                setAddress(event.currentTarget.value);
+              }
+            }}
+          ></input>
+        </header>
+        <div className="flex items-center justify-center">
+          <div className="bg-[#eee] w-2/3 m-10">
+            <div className="w-full bg-[#ddd] p-4 text-white  ">
+              <div className="flex items-center justify-center">
+                <Line
+                  data={chartData}
+                  options={{
+                    responsive: true,
+                    plugins: {
+                      legend: {
+                        position: "top" as const,
+                      },
+                      title: {
+                        display: true,
+                        text: "Treasury holdings",
+                      },
                     },
-                    title: {
-                      display: true,
-                      text: "Treasury holdings",
-                    },
-                  },
-                }}
-              />
-            </div>
-            <div className="m-4 space-x-0.5 flex items-center justify-center text-gray-700 text-xs">
-              <div
-                className={`hover:cursor-pointer p-2 ${
-                  startDate.getTime() === deltaDate(today, -1, 0, 0).getTime()
-                    ? "bg-[#d5af08b8] font-semibold"
-                    : "bg-white"
-                }`}
-                onClick={(el) => {
-                  setStartDate(deltaDate(today, -1, 0, 0));
-                }}
-              >
-                1 year
-              </div>
-              <div
-                className={`hover:cursor-pointer p-2 ${
-                  startDate.getTime() === deltaDate(today, 0, -3, 0).getTime()
-                    ? "bg-[#d5af08b8] font-semibold"
-                    : "bg-white"
-                }`}
-                onClick={() => {
-                  setStartDate(deltaDate(today, 0, -3, 0));
-                }}
-              >
-                3 months
-              </div>
-              <div
-                className={`hover:cursor-pointer p-2 ${
-                  startDate.getTime() === deltaDate(today, 0, -1, 0).getTime()
-                    ? "bg-[#d5af08b8] font-semibold"
-                    : "bg-white"
-                }`}
-                onClick={() => {
-                  setStartDate(deltaDate(today, 0, -1, 0));
-                }}
-              >
-                1 month
-              </div>
-            </div>
-          </div>
-          <div className="p-10">
-            <KpisDisplay newKpis={newKpis} baseKpis={baseKpis} />
-            <AssetsDisplay newAssets={newAssets} baseAssets={baseAssets} />
-          </div>
-        </div>
-        <div
-          className={
-            (openedProduct === undefined ? "p-6 " : "") +
-            "bg-[#eee] w-1/3 m-10 space-y-6"
-          }
-        >
-          {openedProduct === undefined && (
-            <h2 className="text-3xl mb-6">Products</h2>
-          )}
-          {products.map((props, index) =>
-            openedProduct === undefined ? (
-              <Product
-                key={index}
-                opened={false}
-                {...props}
-                toggle={() => setOpenedProduct(index)}
-              />
-            ) : (
-              index === openedProduct && (
-                <Product
-                  key={index}
-                  opened={true}
-                  {...props}
-                  toggle={() => setOpenedProduct(undefined)}
-                  updatePreview={() => setActiveProduct(index)}
-                  resetPreview={() => resetAssets()}
+                  }}
                 />
-              )
-            )
-          )}
+              </div>
+              <div className="m-4 space-x-0.5 flex items-center justify-center text-gray-700 text-xs">
+                <div
+                  className={`hover:cursor-pointer p-2 ${
+                    startDate.getTime() === deltaDate(today, -1, 0, 0).getTime()
+                      ? "bg-[#d5af08b8] font-semibold"
+                      : "bg-white"
+                  }`}
+                  onClick={(el) => {
+                    setStartDate(deltaDate(today, -1, 0, 0));
+                  }}
+                >
+                  1 year
+                </div>
+                <div
+                  className={`hover:cursor-pointer p-2 ${
+                    startDate.getTime() === deltaDate(today, 0, -3, 0).getTime()
+                      ? "bg-[#d5af08b8] font-semibold"
+                      : "bg-white"
+                  }`}
+                  onClick={() => {
+                    setStartDate(deltaDate(today, 0, -3, 0));
+                  }}
+                >
+                  3 months
+                </div>
+                <div
+                  className={`hover:cursor-pointer p-2 ${
+                    startDate.getTime() === deltaDate(today, 0, -1, 0).getTime()
+                      ? "bg-[#d5af08b8] font-semibold"
+                      : "bg-white"
+                  }`}
+                  onClick={() => {
+                    setStartDate(deltaDate(today, 0, -1, 0));
+                  }}
+                >
+                  1 month
+                </div>
+              </div>
+            </div>
+            <div className="p-10">
+              <KpisDisplay newKpis={newKpis} baseKpis={baseKpis} />
+              <AssetsDisplay newAssets={newAssets} baseAssets={baseAssets} />
+            </div>
+          </div>
+          <div
+            className={
+              (openedProduct === undefined ? "p-6 " : "") +
+              "bg-[#eee] w-1/3 m-10 space-y-6"
+            }
+          >
+            {openedProduct === undefined && (
+              <h2 className="text-3xl mb-6">Products</h2>
+            )}
+            {products.map(
+              (props, index) =>
+                (openedProduct === undefined || index === openedProduct) && (
+                  <Product
+                    key={index}
+                    opened={index === openedProduct}
+                    toggle={() =>
+                      setOpenedProduct(
+                        index === openedProduct ? undefined : index
+                      )
+                    }
+                    previewNewKpis={setNewKpis}
+                    previewNewAssets={setNewAssets}
+                    previewNewChartData={previewNewChartData}
+                    resetPreview={() => resetAssets()}
+                    {...props}
+                  />
+                )
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </SimulationReactContext.Provider>
   );
 }
 
@@ -370,47 +325,50 @@ function KpisDisplay({
   baseKpis,
 }: {
   newKpis: Kpis | undefined;
-  baseKpis: Kpis;
+  baseKpis: Kpis | undefined;
 }) {
   const kpis = newKpis || baseKpis;
   return (
     <div className="mb-10 flex justify-between items-center">
       <div className="bg-[#ddd] w-40 pb-4 pt-4 pl-6 pr-6 text-center">
-        <p
-          className={
-            (newKpis === undefined ? "" : "text-[#D5AF08] ") +
-            "text-2xl font-bold"
-          }
-        >
-          {typeof kpis["total value"] !== "undefined" &&
-            "$" + (kpis["total value"] / 1_000_000).toFixed(0) + "m"}
-        </p>
+        {typeof kpis !== "undefined" && (
+          <p
+            className={
+              (newKpis === undefined ? "" : "text-[#D5AF08] ") +
+              "text-2xl font-bold"
+            }
+          >
+            ${(kpis.totalValue / 1_000_000).toFixed(0)}m
+          </p>
+        )}
         <p>Total Value</p>
       </div>{" "}
       <div className="bg-[#ddd] w-40 pb-4 pt-4 pl-6 pr-6 text-center">
-        <p
-          className={
-            (newKpis === undefined ? "" : "text-[#D5AF08] ") +
-            "text-2xl font-bold"
-          }
-        >
-          {typeof kpis.volatility !== "undefined" &&
-            (kpis.volatility * 100).toFixed(0) + "%"}
-        </p>
+        {typeof kpis !== "undefined" && (
+          <p
+            className={
+              (newKpis === undefined ? "" : "text-[#D5AF08] ") +
+              "text-2xl font-bold"
+            }
+          >
+            {(kpis.volatility * 100).toFixed(0)}%
+          </p>
+        )}
         <p>Volatility</p>
       </div>
       <div className="bg-[#ddd] w-40 pb-4 pt-4 pl-6 pr-6 text-center">
-        <p
-          className={
-            (newKpis === undefined ? "" : "text-[#D5AF08] ") +
-            "text-2xl font-bold"
-          }
-        >
-          {typeof kpis["return vs market"] !== "undefined" &&
-            (kpis["return vs market"] === "Infinity"
+        {typeof kpis !== "undefined" && (
+          <p
+            className={
+              (newKpis === undefined ? "" : "text-[#D5AF08] ") +
+              "text-2xl font-bold"
+            }
+          >
+            {kpis.returnVsMarket === "Infinity"
               ? "∞"
-              : ((kpis["return vs market"] as number) * 100).toFixed(0) + "%")}
-        </p>
+              : ((kpis.returnVsMarket as number) * 100).toFixed(0) + "%"}
+          </p>
+        )}
         <p>Return vs ETH</p>
       </div>
     </div>
@@ -478,14 +436,39 @@ function Product({
   tokens,
   opened,
   toggle,
-  updatePreview,
+  previewNewKpis,
+  previewNewAssets,
+  previewNewChartData,
   resetPreview,
 }: ProductDescription & {
   opened: boolean;
   toggle: () => void;
-  updatePreview?: () => void;
-  resetPreview?: () => void;
+  previewNewKpis: (arg0: Kpis) => void;
+  previewNewAssets: (arg0: AssetsBreakdown) => void;
+  previewNewChartData: (arg0: Record<string, number>) => void;
+  resetPreview: () => void;
 }) {
+  const [prdParam, setPrdParam] = useState(20);
+  const { address, startDate } = useContext(SimulationReactContext);
+
+  // Fetch product endpoint
+  const launchPreview = async () => {
+    if (!opened) return;
+
+    const resp = await fetch(
+      `/api/backtest/spread/${address}/${startDate
+        .toISOString()
+        .slice(0, 10)}/${prdParam}`
+    );
+    if (!resp.ok)
+      throw new Error(`Backtest fetch failed with status: ${resp.statusText}`);
+    const { assets, kpis, data } = await resp.json();
+
+    previewNewKpis(kpis);
+    previewNewAssets(assets);
+    previewNewChartData(data);
+  };
+
   return (
     <div>
       <div
@@ -529,18 +512,26 @@ function Product({
           <div className="p-4 space-y-2">
             <h3 className="font-bold">You swap</h3>
             <div className="flex items-center justify-between">
-              <span className="uppercase">Asset</span>
+              <span className="uppercase">Asset %</span>
               <span>
-                <input className="w-[3em] p-2 text-right" placeholder="20%" />
+                <input
+                  className="w-[3em] p-2 text-right"
+                  placeholder="20"
+                  type="number"
+                  min="0"
+                  max="100"
+                  onChange={(e) => setPrdParam(parseInt(e.target.value))}
+                  value={prdParam}
+                />
               </span>
             </div>
             <div className="flex items-center justify-between">
-              <span className="uppercase">Value</span>
+              <span className="uppercase">Value $</span>
               <span>
                 <input
                   className="p-2 text-right"
                   disabled={true}
-                  value={"$12,000,000"}
+                  value="12,000,000"
                 />
               </span>
             </div>
@@ -558,7 +549,7 @@ function Product({
               </span>
             </div>
             <div className="flex items-center justify-between">
-              <span className="uppercase">Value</span>
+              <span className="uppercase">Value $</span>
               <span>
                 <input
                   className="p-2 text-right bg-[#ddd]"
@@ -571,7 +562,7 @@ function Product({
           <div className="pt-8 flex items-center justify-center">
             <button
               className=" py-4 px-8 bg-[#D5AF08] hover:bg-[#444] text-white font-bold"
-              onClick={updatePreview}
+              onClick={launchPreview}
             >
               Review order
             </button>
