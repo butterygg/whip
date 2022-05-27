ef8935bde797db76a65b37dbb802e1eaa31f9010
Jamil Bousquet <jamil.bousquet@gmail.com>
2022-05-27 14:17:23 -0400

    calculating and storing correlations of assets;
    hash: `asset_correlations`;
    key: treasury address

--- scheduler/app/libs/storage_helpers.py
+++ scheduler/app/libs/storage_helpers.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Any, Union
 import redis
 import json
 
@@ -22,16 +22,26 @@ def retrieve_treasuries_metadata() -> list[tuple[str, int]]:
 BALANCES_KEY_TEMPLATE = "{address}_{symbol}"
 
 
+def store_hash_set(
+    set: str,
+    key: str,
+    value: Any,
+    provider: Union[redis.Redis, redis.client.Pipeline] = db,
+):
+    provider.hset(set, key, value)
+
+
 def store_asset_hist_balance(
     treasury_address: str,
     symbol: str,
     asset_hist_balance_json: str,
     provider: Union[redis.Redis, redis.client.Pipeline] = db,
 ):
-    provider.hset(
+    store_hash_set(
         "balances",
         BALANCES_KEY_TEMPLATE.format(address=treasury_address, symbol=symbol),
         asset_hist_balance_json,
+        provider,
     )
 
 
@@ -40,4 +50,14 @@ def store_asset_hist_performance(
     asset_hist_performance_json: str,
     provider: Union[redis.Redis, redis.client.Pipeline] = db,
 ):
-    provider.hset("asset_hist_performance", symbol, asset_hist_performance_json)
+    store_hash_set(
+        "asset_hist_performance", symbol, asset_hist_performance_json, provider
+    )
+
+
+def store_asset_correlations(
+    address: str,
+    asset_correlations_json: str,
+    provider: Union[redis.Redis, redis.client.Pipeline] = db,
+):
+    store_hash_set("asset_correlations", address, asset_correlations_json, provider)
