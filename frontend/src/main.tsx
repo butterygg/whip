f44af950d8cd406a69738fdbb62e73cb5a677c7a
lajarre <alexandre.hajjar@gmail.com>
2022-06-02 16:45:31 +0100

    ✅ Format frontend code (tsc + eslint + prettier)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const root = document.getElementById("root");
+
+if (typeof root !== "undefined" && root !== null) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
