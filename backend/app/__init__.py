c6993a8dcd6bb2642b34bb337d5b4943204380e3
lajarre <alexandre.hajjar@gmail.com>
2022-06-08 17:01:26 +0100

    📈 Add Sentry to backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 import redis
+import sentry_sdk
 
 if "REDIS_TLS_URL" in os.environ:
     db = redis.Redis.from_url(os.environ["REDIS_TLS_URL"], ssl_cert_reqs=None)
@@ -8,3 +9,9 @@ elif "REDIS_URL" in os.environ:
     db = redis.Redis.from_url(os.environ["REDIS_URL"])
 else:
     db = redis.Redis(host="redis")
+
+if "SENTRY_DSN" in os.environ:
+    sentry_sdk.init(
+        os.environ["SENTRY_DSN"],
+        traces_sample_rate=1.0,
+    )
