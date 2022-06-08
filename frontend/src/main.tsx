414b00c90388a4203608a475e76793dc083e506c
lajarre <alexandre.hajjar@gmail.com>
2022-06-08 18:52:19 +0100

    📈 Add Sentry to frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
+
 import App from "./App";
 import "./index.css";
+
+if (import.meta.env.VITE_SENTRY_DSN) {
+  Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
 
 const root = document.getElementById("root");
 
