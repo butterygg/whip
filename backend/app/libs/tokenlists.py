4a2dd4ae1ea87caf5ac09fcc3564ec3a238c2f3d
Jamil Bousquet <jamil.bousquet@gmail.com>
2022-06-17 12:56:12 -0400

    :sparkles: added CMC tokenlist to whitelist

--- backend/app/libs/tokenlists.py
+++ backend/app/libs/tokenlists.py
@@ -1,14 +1,27 @@
-from httpx import Client, Timeout
+from asyncio import gather
+from httpx import AsyncClient, Timeout
 
 from .storage_helpers import store_token_whitelist
 
 
-def get_coingecko_token_list():
+async def get_token_list(token_list: str):
     timeout = Timeout(10.0, read=30.0, connect=15.0)
-    with Client(timeout=timeout) as client:
-        resp = client.get("https://tokens.coingecko.com/uniswap/all.json")
+    async with AsyncClient(timeout=timeout) as client:
+        resp = await client.get(token_list)
 
         tokens = resp.json()["tokens"]
         for token in tokens:
             if token["chainId"] == 1:
                 store_token_whitelist(token["address"])
+
+
+async def get_coingecko_token_list():
+    await get_token_list("https://tokens.coingecko.com/uniswap/all.json")
+
+
+async def get_cmc_token_list():
+    await get_token_list("https://api.coinmarketcap.com/data-api/v3/uniswap/all.json")
+
+
+async def get_all_token_lists():
+    await gather(get_coingecko_token_list(), get_cmc_token_list())
