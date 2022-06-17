b3b8c5dc4834df85db7291d8c4d16d9f700c3f07
Jamil Bousquet <jamil.bousquet@gmail.com>
2022-06-17 12:56:12 -0400

    :sparkles: added CMC tokenlist to whitelist

--- backend/app/libs/tasks/get_assets.py
+++ backend/app/libs/tasks/get_assets.py
@@ -1,3 +1,4 @@
+from asyncio import run
 from datetime import datetime, timedelta
 from functools import reduce
 from json import dumps
@@ -21,7 +22,7 @@ from ..storage_helpers import (
     store_asset_hist_balance,
     store_asset_hist_performance,
 )
-from ..tokenlists import get_coingecko_token_list
+from ..tokenlists import get_all_token_lists
 from ..types import ERC20, Treasury
 
 load_dotenv()
@@ -30,11 +31,11 @@ load_dotenv()
 async def make_treasury(treasury_address: str, chain_id: int) -> Treasury:
     token_whitelist = retrieve_token_whitelist()
     if not token_whitelist:
-        get_coingecko_token_list()
+        await get_all_token_lists()
         token_whitelist = retrieve_token_whitelist()
     return await covalent.get_treasury(
         await covalent.get_treasury_portfolio(treasury_address, chain_id),
-        token_whitelist
+        token_whitelist,
     )
 
 
@@ -342,6 +343,11 @@ def setup_periodic_tasks(sender, **_):
     )
 
 
+@celery_app.on_after_finalize.connect
+def start_whitelist_reload(**_):
+    reload_whitelist.apply_async()
+
+
 @celery_app.task
 def reload_treasuries_data():
     end_date: datetime = today(UTC)
@@ -349,8 +355,6 @@ def reload_treasuries_data():
 
     start = start_date.isoformat()[:10]
     end = end_date.isoformat()[:10]
-
-    get_coingecko_token_list()
 
     for treasury_metadata in retrieve_treasuries_metadata():
         with db.pipeline() as pipe:
@@ -396,3 +400,8 @@ def reload_treasuries_data():
             )
 
             pipe.execute()
+
+
+@celery_app.task
+def reload_whitelist():
+    run(get_all_token_lists())
