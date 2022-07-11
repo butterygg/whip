7aa91cfb8abc9cdd18bad3577c0f1712dcd48de7
lajarre <alexandre.hajjar@gmail.com>
2022-07-11 13:29:47 +0100

    🔥 Simplify Treasury object

--- backend/app/treasury/models.py
+++ backend/app/treasury/models.py
@@ -1,4 +1,3 @@
-import datetime
 from dataclasses import dataclass, field
 
 import pandas as pd
@@ -14,24 +13,9 @@ class ERC20:
 
 
 @dataclass
-class Quote:
-    timestamp: datetime.datetime
-    quote_rate: float
-
-
-@dataclass
-class HistoricalPrice:
-    token_address: str
-    token_name: str
-    token_symbol: str
-    quotes: list[Quote]
-
-
-@dataclass
 class Treasury:
     address: str
     assets: list[ERC20]
-    historical_prices: list[HistoricalPrice]
 
     def get_asset(self, symbol: str):
         return next(asset for asset in self.assets if asset.token_symbol == symbol)
