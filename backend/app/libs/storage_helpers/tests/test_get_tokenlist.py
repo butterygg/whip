4e4b4504234e37e7502c8ade1b0aeffb9136e20e
Jamil Bousquet <jamil.bousquet@gmail.com>
2022-07-22 10:58:33 -0400

    :sparkles: Add Covalent and Tokenlist LP whitelist
    This will add LP pairs to the Whip whitelist.
    Allowing Whip to run analytics on these pairs for a given treasury.
    
    :art: Separate Tokenlist logic from xy=k logic
    :green_heart: Apply Black recommendations
    :sparkles: Add SushiSwap pairs to whitelist
    :green_heart: Apply Isort recommendations
    :white_check_mark: Add pagination test for pairs

---
+++ backend/app/libs/storage_helpers/tests/test_get_tokenlist.py
@@ -0,0 +1,155 @@
+from json import loads
+from json.decoder import JSONDecodeError
+
+import pytest
+from httpx import AsyncClient
+
+from backend.app.libs.storage_helpers.tokenlists import (
+    get_tokenlists,
+    maybe_populate_whitelist,
+    store_and_get_tokenlist_whitelist,
+)
+
+from .conftest import (
+    HTTPStatusError,
+    MockProvider,
+    raise_http_status_error_404,
+    raise_http_status_error_501,
+)
+
+
+class MockResponse:
+    status_code = None
+
+    def raise_for_status(self):
+        return None
+
+    @staticmethod
+    def json():
+        return {
+            "name": "MockGecko",
+            "logoURI": "https://www.mockgecko.com/mock/path/thumb.jpg",
+            "keywords": ["mockfi"],
+            "timestamp": "2022-06-19T11:08:16.098+00:00",
+            "tokens": [
+                {"chainId": 1, "address": "0x6d6f636b5f31"},
+                {"chainId": 1, "address": "0x6d6f636b5f32"},
+            ],
+        }
+
+
+async def mock_get_tokenlist(*_):
+    return MockResponse()
+
+
+@pytest.mark.asyncio
+async def test_get_tokenlists_success(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(AsyncClient, "get", mock_get_tokenlist)
+
+    mocked_provider = MockProvider()
+
+    await store_and_get_tokenlist_whitelist(mocked_provider)
+    assert mocked_provider.db_payload["whitelist"] == {
+        "0x6d6f636b5f31",
+        "0x6d6f636b5f32",
+        "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",  # Native ETH should always be whitelisted
+    }
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "json_resp",
+    [
+        lambda _: {
+            "name": "MockGecko",
+            "logoURI": "https://www.mockgecko.com/mock/path/thumb.jpg",
+            "keywords": ["mockfi"],
+            "timestamp": "2022-06-19T11:08:16.098+00:00",
+            "tokens": [
+                {"chainId": 1, "address": "0x6d6f636b5f31"},
+                {"chainId": 1},
+            ],
+        },
+        lambda _: {
+            "name": "MockGecko",
+            "logoURI": "https://www.mockgecko.com/mock/path/thumb.jpg",
+            "keywords": ["mockfi"],
+            "timestamp": "2022-06-19T11:08:16.098+00:00",
+            "tokens": [
+                {"address": "0x6d6f636b5f31"},
+                {"chainId": 1, "address": "0x6d6f636b5f32"},
+            ],
+        },
+        lambda _: {
+            "name": "MockGecko",
+            "logoURI": "https://www.mockgecko.com/mock/path/thumb.jpg",
+            "keywords": ["mockfi"],
+            "timestamp": "2022-06-19T11:08:16.098+00:00",
+            "toekns": [
+                {"chainId": 1, "address": "0x6d6f636b5f31"},
+                {"chainId": 1, "address": "0x6d6f636b5f32"},
+            ],
+        },
+    ],
+)
+async def test_get_tokenlists_bad_resp_json(monkeypatch: pytest.MonkeyPatch, json_resp):
+    async def async_return(*_):
+        return MockResponse()
+
+    url = "https://tokens.coingecko.com/uniswap/all.json"
+    monkeypatch.setattr(AsyncClient, "get", async_return)
+    monkeypatch.setattr(MockResponse, "json", json_resp)
+    with pytest.raises(KeyError):
+        await get_tokenlists(url)
+
+
+@pytest.mark.asyncio
+async def test_maybe_populate_whitelist_success_preprop(
+    monkeypatch: pytest.MonkeyPatch,
+):
+    monkeypatch.setattr(
+        "backend.app.libs.storage_helpers.tokenlists.retrieve_token_whitelist",
+        lambda _: ["0x6d6f636b5f746f6b656e5f31", "0x6d6f636b5f746f6b656e5f32"],
+        raising=True,
+    )
+    mocked_whitelist = await maybe_populate_whitelist("mocked_provider")
+    assert mocked_whitelist == [
+        "0x6d6f636b5f746f6b656e5f31",
+        "0x6d6f636b5f746f6b656e5f32",
+    ]
+
+
+@pytest.mark.asyncio
+async def test_get_tokenlists_resp_err_404(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(AsyncClient, "get", mock_get_tokenlist)
+    monkeypatch.setattr(MockResponse, "status_code", 404)
+    monkeypatch.setattr(MockResponse, "raise_for_status", raise_http_status_error_404)
+
+    url = "https://tokens.coingecko.com/uniswap/all.json"
+    with pytest.raises(HTTPStatusError) as error:
+        await get_tokenlists(url)
+
+        assert "mocked http status error" in str(error.value)
+
+
+@pytest.mark.asyncio
+async def test_get_tokenlists_resp_err_5xx(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(AsyncClient, "get", mock_get_tokenlist)
+    monkeypatch.setattr(MockResponse, "status_code", 501)
+    monkeypatch.setattr(MockResponse, "raise_for_status", raise_http_status_error_501)
+
+    url = "https://tokens.coingecko.com/uniswap/all.json"
+    with pytest.raises(HTTPStatusError) as error:
+        await get_tokenlists(url)
+
+        assert "mocked http status error" in str(error.value)
+
+
+@pytest.mark.asyncio
+async def test_get_tokenlists_json_err(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(AsyncClient, "get", mock_get_tokenlist)
+    monkeypatch.setattr(MockResponse, "json", lambda _: loads("bad json response"))
+
+    url = "https://tokens.coingecko.com/uniswap/all.json"
+    with pytest.raises(JSONDecodeError):
+        await get_tokenlists(url)
